56e7b1946905d75e813fcd48b0417462368a076f
matveyc1 <matvey.chalapko@bk.ru>
2021-05-06 16:48:44 +0300

    Сделал 2 задание

--- Ex.2.py
+++ Ex.2.py
@@ -1,6 +1,19 @@
-t = int(input("Введите время в секундах:"))
-print(t)
-h = t // 3600
-m = (t % 3600) // 60
-s = (t % 3600) % 60
-print('{0}:{1}:{2}'.format(h, m, s))
+i = 0
+n = 0
+m = 1
+my_l = list(input("Введите элементы: "))
+print(my_l)
+p = len(my_l)
+if p % 2 != 0:
+    p = p - 1
+p = p / 2
+while p > 0:
+    my_l.insert(m, my_l[i])
+    my_l.insert(n, my_l[i+2])
+    my_l.pop(i+2)
+    my_l.pop(i+2)
+    i = i + 2
+    n = n + 2
+    m = m + 2
+    p = p - 1
+print(my_l)
