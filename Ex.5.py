b4081b7025199dd23daf3df05c7f5c8b15432569
matveyc1 <matvey.chalapko@bk.ru>
2021-05-06 16:51:40 +0300

    Сделал 5 задание

--- Ex.5.py
+++ Ex.5.py
@@ -1,12 +1,20 @@
-a = float(input("Введите значение выручки: "))
-print(a)
-b = float(input("Введите значение издержек: "))
-print(b)
-if a > b:
-    print("Рейнтабельность выручки:", "%.0f" % (((a - b) / a) * 100), "%")
-    print("Прибыль")
-elif a < b:
-    print("Убыток")
-c = int(input("Введите количество рабочих:"))
-print(c)
-print("Прибыль одного работника: ", "%.2f" % ((a - b) / c))
+reiting = [7, 6, 4, 2, 1]
+mark = int(input("Хотите оценить?(1 - да, 0 - выход): "))
+while mark != 0:
+    mark = int(input("Поставьте оценку от 1 до 9: "))
+    if mark > 9:
+        print("Упс, похоже вы нас перехвалили!:D Хватит и 9!")
+        reiting.append(9)
+        reiting.sort(reverse=True)
+        print(f"Текущий список - {reiting}")
+    elif mark < 1:
+        print("Это слишком мало, поэтому остановимся на единичке. Надеемся, что в следующий раз вам понравиться! :С")
+        reiting.append(1)
+        reiting.sort(reverse=True)
+        print(f"Текущий список - {reiting}")
+    else:
+        reiting.append(mark)
+        reiting.sort(reverse=True)
+        print(f"Текущий список - {reiting}")
+        print("До свидания! :)")
+    mark = int(input("Поставьте оценку от 1 до 9: "))
