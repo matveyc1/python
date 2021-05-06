a8273ddfe54adf53228c19e9cade2e1d68602791
matveyc1 <matvey.chalapko@bk.ru>
2021-05-06 16:48:30 +0300

    Сделал 1 задание

--- Ex. 1.py
+++ Ex. 1.py
@@ -1,10 +1,15 @@
-a = "Майкл Майерс"
-b = 123
-c = 12.3
-print(a, ' ', b, ' ', c, ' ', b > c)
-name = input("Введите имя: ")
-age = int(input("Введите возраст: "))
-lol = float(input("2/5 = "))
-print(name)
-print(age)
-print(lol)
+result_list = [12, 5.5, 'This will be text!']
+i = 0
+n = len(result_list)
+while n > 0:
+    tipe = type(result_list[i])
+    if tipe == str:
+        tipe = "строковый"
+    elif tipe == int:
+        tipe = "целочисленный"
+    elif tipe == float:
+        tipe = "вещественный"
+    print(f"Элемент списка '{result_list[i]}' имеет тип {tipe}")
+    n = n - 1
+    i = i + 1
+
