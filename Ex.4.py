36012ecea07d31fac525061fe2b1092b4098da85
matveyc1 <matvey.chalapko@bk.ru>
2021-05-06 16:50:52 +0300

    Сделал 3 задание

--- Ex.4.py
+++ Ex.4.py
@@ -1,9 +1,13 @@
-a = 0
-s = int(input("Введите число: "))
-print(s)
-while s > 0:
-    b = s % 10
-    if b > a:
-        a = b
-    s = s // 10
-print("Наибольщая цифра числа: ", a)
+my_l = input("Введите строку: ")
+my_str = []
+num = 1
+i = 0
+my_str = my_l.split()
+long = len(my_str)
+while long > 0:
+    print(f"{num}. {my_str[i]}")
+    i += 1
+    num += 1
+    long -= 1
+
+
